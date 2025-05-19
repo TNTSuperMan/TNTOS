fb15b01fb8165893f185fc3244e5ba2be8c81fcc
TNTSuperMan <abworld@outlook.jp>
2025-05-19 19:04:05 +0900

    サブディレクトリとか面倒くさくてスクリプト化

---
+++ make.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env bun
+import { readdirSync } from "fs"
+import { rm } from "fs/promises"
+import { resolve } from "path"
+import { spawn } from "bun"
+
+const [,, option] = process.argv;
+
+switch(option){
+  case "clean":
+    await Promise.all(readdirSync(".", { recursive: true }).map(e=>{
+      if(typeof e != "string") return;
+      if(e.endsWith(".o") || e.endsWith(".d"))
+        return rm(resolve(__dirname, "kernel", e));
+    })); break;
+  case "link":
+    const objects = readdirSync(".", { recursive: true })
+      .filter(e=>typeof e == "string" && e.endsWith(".o")) as string[];
+    await spawn({cmd:["ld.lld", process.env.LDFLAGS??"",
+        "--entry", "KernelMain",
+        "-z", "norelro",
+        "--image-base", "0x100000",
+        "--static",
+        "-o", "kernel.elf",
+        ...objects,
+        "-lc", "-lc++", "-lc++abi", "-lm", "-lfreetype"]}).exited;
+    break;
+}
