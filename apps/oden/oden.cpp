a45e9e89f0166519df1ba4fe1cf897fbff33370e
TNTSuperMan <abworld@outlook.jp>
2025-06-10 04:28:27 +0900

    odenのAPIをモジュール化

--- apps/oden/oden.cpp
+++ apps/oden/oden.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include "../syscall.h"
 #include "duktape/duktape.h"
+#include "duktape/duk_module_duktape.h"
 
 #include "api/file.hpp"
 
@@ -13,6 +14,43 @@ duk_ret_t print(duk_context *ctx) {
     duk_pop(ctx);
   }
   return 0;
+}
+
+duk_ret_t mod_search(duk_context *ctx) {
+  const char *module_id = duk_require_string(ctx, -4);
+  if(strcmp(module_id, "oden") == 0){
+
+    duk_push_object(ctx);
+  
+    duk_push_c_function(ctx, readFile, 1);
+    duk_put_prop_string(ctx, -2, "readFile");
+  
+    duk_push_c_function(ctx, writeFile, 2);
+    duk_put_prop_string(ctx, -2, "writeFile");
+
+    duk_put_prop_string(ctx, -2, "exports");
+
+    duk_pop(ctx);
+    return 0;
+  }else{
+    SyscallResult res = SyscallOpenFile(module_id, 0);
+    if(res.error){
+      fprintf(stderr, "Failed to resolve %s: %s\n", module_id, strerror(res.error));
+      duk_error(ctx, DUK_ERR_ERROR, "Failed to resolve file");
+      return 0;
+    }
+    const int fd = res.value;
+    size_t fs;
+    res = SyscallMapFile(fd, &fs, 0);
+    if(res.error){
+      duk_error(ctx, DUK_ERR_ERROR, strerror(res.error));
+      return 0;
+    }
+
+    const char* module = reinterpret_cast<char*>(res.value);
+    duk_push_string(ctx, module);
+    return 1;
+  }
 }
 
 int main(int argc, char** argv) {
@@ -37,14 +75,15 @@ int main(int argc, char** argv) {
   
   duk_context *ctx = duk_create_heap_default();
 
+  duk_module_duktape_init(ctx);
+
   duk_push_c_function(ctx, print, DUK_VARARGS);
   duk_put_global_string(ctx, "print");
   
-  duk_push_c_function(ctx, readFile, 1);
-  duk_put_global_string(ctx, "readFile");
-  
-  duk_push_c_function(ctx, writeFile, 2);
-  duk_put_global_string(ctx, "writeFile");
+  duk_get_global_string(ctx, "Duktape");
+  duk_push_c_function(ctx, mod_search, 4);
+  duk_put_prop_string(ctx, -2, "modSearch");
+  duk_pop(ctx);
 
   if(duk_peval_string(ctx, script)){
     printf("%s\n", duk_safe_to_string(ctx, -1));
