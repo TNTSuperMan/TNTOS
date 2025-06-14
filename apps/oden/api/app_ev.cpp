e28e2b5623c899d178e1780d8da6b1afd80a54ab
TNTSuperMan <abworld@outlook.jp>
2025-06-14 22:30:20 +0900

    duk_push_numberをintにして最適化？

--- apps/oden/api/app_ev.cpp
+++ apps/oden/api/app_ev.cpp
@@ -18,32 +18,32 @@ duk_ret_t readEvent(duk_context *ctx) {
       duk_push_string(ctx, "mouse_move");
       duk_put_prop_string(ctx, -2, "type");
 
-      duk_push_number(ctx, ev[0].arg.mouse_move.x);
+      duk_push_int(ctx, ev[0].arg.mouse_move.x);
       duk_put_prop_string(ctx, -2, "x");
 
-      duk_push_number(ctx, ev[0].arg.mouse_move.y);
+      duk_push_int(ctx, ev[0].arg.mouse_move.y);
       duk_put_prop_string(ctx, -2, "y");
 
-      duk_push_number(ctx, ev[0].arg.mouse_move.dx);
+      duk_push_int(ctx, ev[0].arg.mouse_move.dx);
       duk_put_prop_string(ctx, -2, "dx");
 
-      duk_push_number(ctx, ev[0].arg.mouse_move.dy);
+      duk_push_int(ctx, ev[0].arg.mouse_move.dy);
       duk_put_prop_string(ctx, -2, "dy");
 
-      duk_push_number(ctx, ev[0].arg.mouse_move.buttons);
+      duk_push_uint(ctx, ev[0].arg.mouse_move.buttons);
       duk_put_prop_string(ctx, -2, "buttons");
       break;
     case AppEvent::kMouseButton:
       duk_push_string(ctx, "mouse_button");
       duk_put_prop_string(ctx, -2, "type");
 
-      duk_push_number(ctx, ev[0].arg.mouse_button.x);
+      duk_push_int(ctx, ev[0].arg.mouse_button.x);
       duk_put_prop_string(ctx, -2, "x");
 
-      duk_push_number(ctx, ev[0].arg.mouse_button.y);
+      duk_push_int(ctx, ev[0].arg.mouse_button.y);
       duk_put_prop_string(ctx, -2, "y");
 
-      duk_push_number(ctx, ev[0].arg.mouse_button.button);
+      duk_push_int(ctx, ev[0].arg.mouse_button.button);
       duk_put_prop_string(ctx, -2, "button");
       break;
     case AppEvent::kTimerTimeout:
@@ -53,17 +53,17 @@ duk_ret_t readEvent(duk_context *ctx) {
       duk_push_number(ctx, ev[0].arg.timer.timeout);
       duk_put_prop_string(ctx, -2, "timeout");
 
-      duk_push_number(ctx, ev[0].arg.timer.value);
+      duk_push_int(ctx, ev[0].arg.timer.value);
       duk_put_prop_string(ctx, -2, "value");
       break;
     case AppEvent::kKeyPush:
       duk_push_string(ctx, "keypush");
       duk_put_prop_string(ctx, -2, "type");
 
-      duk_push_number(ctx, ev[0].arg.keypush.modifier);
+      duk_push_uint(ctx, ev[0].arg.keypush.modifier);
       duk_put_prop_string(ctx, -2, "modifier");
 
-      duk_push_number(ctx, ev[0].arg.keypush.keycode);
+      duk_push_uint(ctx, ev[0].arg.keypush.keycode);
       duk_put_prop_string(ctx, -2, "keycode");
 
       duk_push_lstring(ctx, (const char*)&ev[0].arg.keypush.ascii, 1);
--- apps/oden/api/timer.cpp
+++ apps/oden/api/timer.cpp
@@ -2,8 +2,8 @@
 #include "../../syscall.h"
 
 duk_ret_t createTimer(duk_context *ctx) {
-  unsigned int type = duk_require_number(ctx, -3);
-  int value = duk_require_number(ctx, -2);
+  unsigned int type = duk_require_uint(ctx, -3);
+  int value = duk_require_int(ctx, -2);
   unsigned long ms = duk_require_number(ctx, -1);
 
   const auto timeout = SyscallCreateTimer(type, value, ms);
