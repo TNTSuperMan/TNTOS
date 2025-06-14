8dc4564cdf75c262228c866afea9d286e898ed9f
TNTSuperMan <abworld@outlook.jp>
2025-06-14 18:31:18 +0900

    待ってnewlib_supportに書くべきだったかもしれない ということで時刻系の色々実装はnewlib_support.cに移動

--- apps/newlib_support.c
+++ apps/newlib_support.c
@@ -1,6 +1,7 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -99,4 +100,60 @@ ssize_t write(int fd, const void* buf, size_t count) {
 
 void _exit(int status) {
   SyscallExit(status);
+}
+
+int is_leap_year(int y){
+  return (y % 4 == 0 && y % 100 != 0) || (y % 400 == 0);
+}
+
+int gettimeofday(struct timeval *tv, void* tz) {
+  EFI_TIME t;
+  SyscallGetTime(&t);
+
+  // 時刻構造体 to UNIXエポック?(Copilotさんありがと!)
+  // TODO 表示が4年ぐらうずれる、要微調整
+
+  int days = 0;
+  for(int y = 1970; y < t.Year; y++) {
+    if(is_leap_year(y)){
+      days += 366;
+    }else{
+      days += 365;
+    }
+  }
+
+  for(int m = 1; m < t.Month; m++) {
+    switch(m){
+      case 1:  days += 31;
+      case 2:
+        if(is_leap_year(t.Year)){
+          days += 29;
+        }else{
+          days += 28;
+        }
+      case 3:  days += 31;
+      case 4:  days += 30;
+      case 5:  days += 31;
+      case 6:  days += 30;
+      case 7:  days += 31;
+      case 8:  days += 31;
+      case 9:  days += 30;
+      case 10: days += 31;
+      case 11: days += 30;
+      case 12: days += 31;
+    }
+  }
+
+  days += t.Day - 1;
+
+  __time_t seconds = days * 86400ULL;
+
+  seconds += t.Hour * 3600;
+  seconds += t.Minute * 60;
+  seconds += t.Second;
+
+  tv->tv_sec = seconds;
+  tv->tv_usec = t.Nanosecond / 1000;
+  
+  return 0;
 }
