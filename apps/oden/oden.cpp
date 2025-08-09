cc7e18ee10c974fe725b18ca974ca6d973721965
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-09 15:59:18 +0900

    odenのargc分岐を==のifにする

--- apps/oden/oden.cpp
+++ apps/oden/oden.cpp
@@ -6,27 +6,29 @@
 #include "context.hpp"
 
 int main(int argc, char** argv) {
-  if(argc != 2){
+  if(argc == 1){
+    // TODO: REPL
+  }else if(argc == 2){
+    auto res = SyscallOpenFile(argv[1], 0);
+    if(res.error){
+      fprintf(stderr, "%s: %s\n", strerror(res.error), argv[1]);
+      return 1;
+    }
+    const int fd = res.value;
+    size_t fs;
+    res = SyscallMapFile(fd, &fs, 0);
+    if(res.error){
+      fprintf(stderr, "%s\n", strerror(res.error));
+      return 1;
+    }
+
+    const char* script = reinterpret_cast<char*>(res.value);
+
+    OdenContext* ctx = new OdenContext();
+    ctx->eval(script);
+  }else{
     printf("usage: oden [path]\n");
-    return 0;
   }
-  auto res = SyscallOpenFile(argv[1], 0);
-  if(res.error){
-    fprintf(stderr, "%s: %s\n", strerror(res.error), argv[1]);
-    return 1;
-  }
-  const int fd = res.value;
-  size_t fs;
-  res = SyscallMapFile(fd, &fs, 0);
-  if(res.error){
-    fprintf(stderr, "%s\n", strerror(res.error));
-    return 1;
-  }
-
-  const char* script = reinterpret_cast<char*>(res.value);
-
-  OdenContext* ctx = new OdenContext();
-  ctx->eval(script);
   
   return 0;
 }
